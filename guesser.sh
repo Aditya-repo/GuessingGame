510e0650b179f7d21bc3798cf447bfd224447017
Aditya Poddar <adityapoddar@pop-os.localdomain>
2020-06-13 10:02:05 +0530

    first trial submission

---
+++ guesser.sh
@@ -0,0 +1,29 @@
+function check_if_number {
+	if [[ $1 =~ ^[0-9]+$ ]]
+	    then
+		return 1
+	fi
+	return 0
+}
+
+filecount=`ls|wc -l`
+echo "Guess the number of files in current directory"
+while read -p "yor guess:" number
+    do
+       check_if_number $number
+       if [ $? -eq 0 ]
+          then
+              echo "enter only numbers"
+              continue
+       fi
+       if [ $number -eq $filecount ]
+        then
+            echo "congrats correct guess"
+            break
+        elif [ $number -lt $filecount ]
+           then
+               echo "guess too low"
+        else
+               echo "guess too high"
+        fi
+    done
